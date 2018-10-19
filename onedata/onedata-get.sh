0dcd8ee116ba627a89123ac457bd675b4120c05d
gvdoord <g.vandenOord@gmail.com>
2018-10-19 15:00:11 +0200

    fixed getter script

--- onedata/onedata-get.sh
+++ onedata/onedata-get.sh
@@ -1,26 +1,11 @@
-#!/usr/bin/env cwl-runner
+#!/bin/bash
 
-cwlVersion: v1.0
-class: CommandLineTool
-baseCommand: /opt/onedata-get.sh
-arguments: [$(inputs.source.path), $(inputs.dest)]
-hints:
-  DockerRequirement:
-    dockerImageId: ewatercycle/oneclient
-requirements:
-  EnvVarRequirement:
-    envDef:
-      ONECLIENT_ACCESS_TOKEN: MDAxNWxvY2F00aW9uIG9uZXpvbmUKMDAzMGlkZW500aWZpZXIgMjA3NWI4NWZhZjM4OThmMTUzYmIzMGE2MDQ00NjI5NDcKMDAxYWNpZCB00aW1lIDwgMTU3MTMxMzA2MQowMDJmc2lnbmF00dXJlIIBSX02WEVzWhvWBKnGHuP6gmhosMr6Bf302gsl3NKvKV1Cg
-      ONECLIENT_PROVIDER_HOST: oneprovider-cnaf.datahub.egi.eu
-inputs:
-  source:
-    type: File
-  dest:
-    type: string
-  token:
-    type: string
-  provider:
-    type: string
-outputs:
-  status:
-    type: stdout
+SRC=$1
+DST=$2
+
+MOUNTPOINT=/mnt/onedata
+oneclient $MOUNTPOINT
+
+cp -r $MOUNTPOINT/$SRC $DST/
+
+oneclient -u $MOUNTPOINT
